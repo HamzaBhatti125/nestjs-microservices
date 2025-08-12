9e1475d86d4bbee411784acdd4e213810d233c73
HamzaBhatti125 <hbhati006@gmail.com>
2025-08-12 17:01:19 +0500

    feat: two microservices implemented

---
+++ apps/auth/src/auth.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@nestjs/common';
+import { UserDocument } from './users/models/user.schema';
+import { Response } from 'express';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async login(user: UserDocument, response: Response) {
+    const tokenPayload = {
+      userId: user._id.toHexString(),
+    };
+
+    const expires = new Date();
+    const expiresIn = +this.configService.get<string>('JWT_EXPIRES_IN');
+    expires.setSeconds(expires.getSeconds() + expiresIn);
+
+    const token = this.jwtService.sign(tokenPayload);
+
+    response.cookie('Authentication', token, {
+      httpOnly: true,
+      expires,
+      sameSite: 'lax',
+      secure: false, // Set to true in production with HTTPS
+    });
+  }
+}
---
+++ apps/auth/src/users/users.service.ts
@@ -0,0 +1,53 @@
+import {
+  Injectable,
+  UnauthorizedException,
+  UnprocessableEntityException,
+} from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UserRepository } from './user.repository';
+import * as bcrypt from 'bcryptjs';
+import { GetUserDto } from './dto/get-user.dto';
+
+@Injectable()
+export class UsersService {
+  constructor(private readonly userRepository: UserRepository) {}
+
+  async create(createUserDto: CreateUserDto) {
+    await this.validateCreateUserDto(createUserDto);
+
+    const hashedPassword = await bcrypt.hash(createUserDto.password, 10);
+    const user = { ...createUserDto, password: hashedPassword };
+    return this.userRepository.create(user);
+  }
+
+  private async validateCreateUserDto(createUserDto: CreateUserDto) {
+    try {
+      await this.userRepository.findOne({ email: createUserDto.email });
+    } catch {
+      return;
+    }
+
+    throw new UnprocessableEntityException(
+      'User with this email already exists',
+    );
+  }
+
+  async verifyUser(email: string, password: string): Promise<any> {
+    const user = await this.userRepository.findOne({ email });
+    if (user && (await bcrypt.compare(password, user.password))) {
+      return user;
+    }
+
+    throw new UnauthorizedException('Invalid credentials');
+  }
+
+  async getUser(getUserDto: GetUserDto) {
+    try {
+      const user = await this.userRepository.findOne(getUserDto);
+      return user;
+    } catch (error) {
+      console.error('Error finding user:', error.message);
+      throw error;
+    }
+  }
+}
